d9157f823db9c8c448c6b1d4543297d4c2234f5e
Moazam05 <salmanmoazam08@gmail.com>
2024-02-16 19:04:30 +0500

    payload

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -36,6 +36,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    role: req.body.role,
+    gender: req.body.gender,
   });
 
   createSendToken(newUser, 201, res);
