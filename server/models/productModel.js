0a9b390a744415d245bf934f84cf6fee050bfa9f
Moazam05 <salmanmoazam08@gmail.com>
2024-02-17 20:51:11 +0500

    product model

---
+++ server/models/productModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product must have a name"],
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product must have a description"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Product must have a price"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Product must have a stock"],
+    },
+    category: {
+      type: String,
+      required: [true, "Product must have a category"],
+    },
+    image: {
+      type: String,
+      required: [true, "Product must have an image"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
