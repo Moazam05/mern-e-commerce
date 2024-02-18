059d2e02e0d72e3aa7133e0309dd068c1ae2c16d
Moazam05 <salmanmoazam08@gmail.com>
2024-02-18 15:00:04 +0500

    products api

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,6 +4,46 @@ const Product = require("../models/productModel");
 const checkRequiredFields = require("../utils/requiredFieldError");
 const { rm } = require("fs");
 
+exports.getLatestProducts = catchAsync(async (req, res, next) => {
+  const latestProducts = await Product.find().sort({ createdAt: -1 }).limit(5);
+
+  res.status(200).json({
+    status: "success",
+    data: latestProducts,
+  });
+});
+
+exports.getCategories = catchAsync(async (req, res, next) => {
+  const categories = await Product.find().distinct("category");
+
+  res.status(200).json({
+    status: "success",
+    data: categories,
+  });
+});
+
+exports.getAllProducts = catchAsync(async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    status: "success",
+    results: products.length,
+    data: products,
+  });
+});
+
+exports.getProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) return next(new AppError("Product not found", 404));
+
+  res.status(200).json({
+    status: "success",
+    data: product,
+  });
+});
+
+// TODO: ADMIN ONLY
 exports.createProduct = catchAsync(async (req, res, next) => {
   const { name, description, price, stock, category } = req.body;
   const image = req.file;
@@ -36,5 +76,77 @@ exports.createProduct = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: "success",
     data: newProduct,
+  });
+});
+
+exports.updateProduct = catchAsync(async (req, res, next) => {
+  const { name, description, price, stock, category } = req.body;
+  const image = req.file;
+
+  if (!image) return next(new AppError("Image is required", 400));
+
+  // 1) Check required fields
+  const requiredFields = ["name", "description", "price", "stock", "category"];
+  const missingFields = checkRequiredFields(req.body, requiredFields);
+
+  if (missingFields.length > 0) {
+    rm(image.path, (err) => {
+      console.log("Deleted image file after missing fields error");
+      if (err) console.log(err);
+    });
+    return next(
+      new AppError(`Missing fields: ${missingFields.join(", ")}`, 400)
+    );
+  }
+
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    rm(image.path, (err) => {
+      console.log(`Deleted image file after missing fields error`.bgRed);
+      if (err) console.log(err);
+    });
+    return next(new AppError("Product not found", 404));
+  }
+
+  rm(product.image, (err) => {
+    console.log(`Deleted old image file`.bgRed);
+    if (err) console.log(err);
+  });
+
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+      description,
+      price,
+      stock,
+      category,
+      image: image.path,
+    },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: "success",
+    data: updatedProduct,
+  });
+});
+
+exports.deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) return next(new AppError("Product not found", 404));
+
+  rm(product.image, (err) => {
+    console.log(`Deleted image file`.bgRed);
+    if (err) console.log(err);
+  });
+
+  await Product.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -6,10 +6,20 @@ const singleUpload = require("../middleware/multer");
 
 const router = express.Router();
 
+// PUBLIC ROUTES
+
+router.get("/categories", productController.getCategories);
+router.get("/", productController.getAllProducts);
+router.get("/latest", productController.getLatestProducts);
+router.get("/:id", productController.getProduct);
+
 // PROTECTED ROUTES
 router.use(authController.protect);
 
 // create product
 router.post("/", onlyAdmin, singleUpload, productController.createProduct);
+router
+  .patch("/:id", onlyAdmin, singleUpload, productController.updateProduct)
+  .delete("/:id", onlyAdmin, productController.deleteProduct);
 
 module.exports = router;
