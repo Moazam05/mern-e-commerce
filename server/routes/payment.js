ed128c6d67b45ac97a4a0289be6422d357d672bd
Moazam05 <salmanmoazam08@gmail.com>
2024-03-23 13:22:59 +0500

    protected routes coupon

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,16 +1,17 @@
 const express = require("express");
 // Custom Imports
-const authController = require("../controllers/authController");
 const paymentController = require("../controllers/paymentController");
+const { onlyAdmin } = require("../middleware/auth");
 
 const router = express.Router();
 
-// PROTECTED ROUTES
-// router.use(authController.protect);
-
 // Payment ROUTES
-router.post("/new", paymentController.newCoupon);
 router.get("/discount", paymentController.getDiscount);
+
+router.use(onlyAdmin);
+
+// ADMIN ONLY
+router.post("/new", paymentController.newCoupon);
 router.get("/all", paymentController.getAllCoupons);
 router.delete("/delete/:id", paymentController.deleteCoupon);
 
